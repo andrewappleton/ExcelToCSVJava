201f4f06d1d2213a42d01e8245a9fef651b05f4f
Andrew Appleton <appleton.accounts@gmail.com>
2021-10-26 18:00:34 +0200

    Added logging TODO: Write log file to disk

--- src/main/java/com/genesys/ara/ExcelToCSV.java
+++ src/main/java/com/genesys/ara/ExcelToCSV.java
@@ -4,6 +4,7 @@ import com.codoid.products.exception.FilloException;
 import com.codoid.products.fillo.Connection;
 import com.codoid.products.fillo.Fillo;
 import com.codoid.products.fillo.Recordset;
+import org.pmw.tinylog.Logger;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
@@ -12,16 +13,18 @@ import java.util.Properties;
 public class ExcelToCSV {
 
     static String PROPS_FILE = "./ExcelToCSVJava.properties";
+    static String TEXT_QUALIFIER = "\"";
+
+    boolean showExcelOutput = false;
     boolean overwriteCsv = true;
     boolean useTextQualifier = false;
-    String textQualifier = "\"";
     String delimiter = ",";
 
     public ExcelToCSV(String excelFilename) {
         checkProperties();
-        System.out.println(String.format("Delimiter: %s",delimiter));
-        System.out.println(String.format("Using Text Qualifier: %s",useTextQualifier));
-        System.out.println(String.format("Overwrite CSV file: %s",overwriteCsv));
+        Logger.info(String.format("Delimiter: %s",delimiter));
+        Logger.info(String.format("Using Text Qualifier: %s",useTextQualifier));
+        Logger.info(String.format("Overwrite CSV file: %s",overwriteCsv));
         convert(excelFilename);
     }
 
@@ -29,20 +32,22 @@ public class ExcelToCSV {
         try {
             Properties prop = new Properties();
             prop.load(new FileReader(new File(".").getCanonicalPath() + File.separator + PROPS_FILE));
-            System.out.println(String.format("Found properties file: %s",PROPS_FILE));
+            Logger.info(String.format("Found properties file: %s",PROPS_FILE));
             delimiter = prop.getProperty("delimiter",delimiter);
             overwriteCsv = "true".equals(prop.getProperty("overwrite-csv",
                     String.valueOf(overwriteCsv)));
             useTextQualifier = "true".equals(prop.getProperty("use-text-qualifier",
                     String.valueOf(useTextQualifier)));
+            showExcelOutput = "true".equals(prop.getProperty("show-excel-output",
+                    String.valueOf(showExcelOutput)));
         } catch (IOException e) {
-            System.err.println(String.format("No properties file named %s found, using defaults",PROPS_FILE));
+            Logger.warn(String.format("No properties file named %s found, using defaults",PROPS_FILE));
         }
     }
 
     private boolean fileExists(String filename) {
         File f = new File(filename);
-        System.out.println(String.format("Checking for existence of file %s",filename));
+        Logger.info(String.format("Checking for existence of file %s",filename));
         return f.exists();
     }
 
@@ -56,19 +61,20 @@ public class ExcelToCSV {
         String query = "";
         String csvRow = "";
         String currRecord = "";
+        int csvWriteCount = 0;
         try {
             if (!overwriteCsv && fileExists(csvFilename)) {
-                System.err.println("ERROR: CSV file exists and should not be overwriiten!");
+                Logger.error("ERROR: CSV file exists and should not be overwritten!");
                 return;
             }
             os = new FileOutputStream(csvFilename);
             osWriter = new OutputStreamWriter(os, StandardCharsets.UTF_8);
             connection = fillo.getConnection(excelFilename);
-            System.out.println(String.format("Found Excel file: %s",excelFilename));
+            Logger.info(String.format("Found Excel file: %s",excelFilename));
             for (String table : connection.getMetaData().getTableNames()) {
                 query = String.format("select * from %s", table);
                 recordset = connection.executeQuery(query);
-                System.out.println(String.format("Received %d records!", recordset.getCount()));
+                Logger.info(String.format("Received %d records!", recordset.getCount()));
                 while (recordset.next()) {
                     for (String fieldName : recordset.getFieldNames()) {
                         currRecord = useTextQualifier ?
@@ -77,49 +83,49 @@ public class ExcelToCSV {
                         csvRow += String.format("%s%s", currRecord, delimiter);
                     }
                     csvRow = csvRow.substring(0, csvRow.length() - 1);
-                    System.out.println(csvRow);
+                    if (showExcelOutput)
+                        System.out.println(csvRow);
                     csvRow += "\n";
                     osWriter.append(csvRow);
+                    csvWriteCount ++;
                     csvRow = "";
                 }
             }
+            Logger.info(String.format("Wrote %d records to %s file",csvWriteCount,csvFilename));
             recordset.close();
             connection.close();
         } catch (FilloException e) {
-            System.err.println(e.getMessage());
+            Logger.error(e.getMessage());
             e.printStackTrace();
         } catch (FileNotFoundException e) {
-            System.err.println("ERROR: Could not write output CSV file.");
+            Logger.error("ERROR: Could not write output CSV file.");
         } catch (IOException e) {
-            System.err.println("ERROR: Could not append to CSV file");
+            Logger.error("ERROR: Could not append to CSV file");
         }
         finally {
             try {
                 if (osWriter != null) osWriter.close();
             } catch (IOException e) {
-                System.err.println("ERROR: " + e.getMessage());
+                Logger.error("ERROR: " + e.getMessage());
             }
             try {
                 os.close();
             } catch (IOException e) {
-                System.err.println("ERROR: " + e.getMessage());
+                Logger.error("ERROR: " + e.getMessage());
             }
         }
     }
 
     private String wrapField(String field) {
-        return textQualifier + field + textQualifier;
+        return TEXT_QUALIFIER + field + TEXT_QUALIFIER;
     }
 
     public static void main(String [] args) {
-        System.out.println("Java Excel CSV Converter 1.0.0");
+        Logger.info("Java Excel CSV Converter 1.0.0");
         if (args.length != 1) {
-            System.out.println("Usage: java -jar ExcelToCSV.jar <filename.xlsx>");
+            Logger.info("Usage: java -jar ExcelToCSV.jar <filename.xlsx>");
         } else {
             new ExcelToCSV(args[0]);
         }
     }
 }
-
-
-///TODO: Add logging framework... log4j?
